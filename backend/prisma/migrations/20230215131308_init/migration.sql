c2213d29fae3916bbb682051e144d649cda1844b
Dominique Chin-Jones <domecj13@gmail.com>
2023-02-15 08:24:37 -0500

    create password column; add bcrypt; test endpoints

--- backend/prisma/migrations/20230215131308_init/migration.sql
+++ backend/prisma/migrations/20230215131308_init/migration.sql
@@ -2,6 +2,7 @@
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "first_name" TEXT NOT NULL,
     "last_name" TEXT NOT NULL,
--- backend/src/user/user.ts
+++ backend/src/user/user.ts
@@ -2,24 +2,25 @@ import { Request, Response } from "express";
 import { prisma } from "..";
 
 export async function getUser(req: Request, res: Response) {
+    // TODO: Error handling
     const user = await prisma.user.findUnique({
         where: {
             username: "cmdcjones",
         },
     });
-    console.log(user);
     res.status(200).send(`Currently viewing user: ${user?.username}`);
 }
 
 export async function createUser(req: Request, res: Response) {
+    // TODO: Error handling
     const newUser = await prisma.user.create({
         data: {
             username: "cmdcjones",
+            password: "plaintextpass",
             email: "test@test",
             first_name: "Dom",
             last_name: "Jones",
         },
     });
-
-    console.log(`User added: ${newUser?.username}`);
+    res.status(200).json({ username: newUser?.username, password: newUser?.password });
 }
