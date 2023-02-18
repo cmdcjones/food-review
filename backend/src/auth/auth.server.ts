8fa75092f5777f1187d23bc95bf67c776c3e9a43
Dominique Chin-Jones <domecj13@gmail.com>
2023-02-18 18:58:28 -0500

    fix: introduced else clause to fix header error

--- backend/src/auth/auth.server.ts
+++ backend/src/auth/auth.server.ts
@@ -14,35 +14,35 @@ export async function createUser(req: Request, res: Response) {
         res.status(400).json({ message: "Please enter a valid email address." });
     } else if (!first_name || !last_name) {
         res.status(400).json({ message: "Please enter a valid first name and last name." });
-    }
+    } else {
 
-    try {
-        const checkUserExists = await Promise.all([
-                prisma.user.findUnique({ where: { username: username, }, }),
-                prisma.user.findUnique({ where: { email: email, }, }),
-        ]);
+        try {
+            const checkUserExists = await Promise.all([
+                    prisma.user.findUnique({ where: { username: username, }, }),
+                    prisma.user.findUnique({ where: { email: email, }, }),
+            ]);
 
-        if (checkUserExists.some(data => data !== null)) {
-            res.status(400).json({ message: "A user with this username/email already exists." });
+            if (checkUserExists.some(data => data !== null)) {
+                res.status(400).json({ message: "A user with this username/email already exists." });
+            } else {
+                const hashedResult = await bcrypt.hash(password, SALT_ROUNDS);
+
+                const newUser = await prisma.user.create({
+                    data: {
+                        username: username,
+                        password: hashedResult,
+                        email: email,
+                        first_name: first_name,
+                        last_name: last_name,
+                    },
+                });
+
+                res.status(200).json({ username: newUser?.username, password: newUser?.password });
+            }
+        } catch (err) {
+            res.status(500).json({ message: "There was an error creating a new user record.", err });
         }
-
-        const hashedResult = await bcrypt.hash(password, SALT_ROUNDS);
-
-        const newUser = await prisma.user.create({
-            data: {
-                username: username,
-                password: hashedResult,
-                email: email,
-                first_name: first_name,
-                last_name: last_name,
-            },
-        });
-
-        res.status(200).json({ username: newUser?.username, password: newUser?.password });
-    } catch (err) {
-        res.status(500).json({ message: "There was an error creating a new user record.", err });
     }
-
 }
 
 export async function loginUser(req: Request, res: Response) {
@@ -50,21 +50,22 @@ export async function loginUser(req: Request, res: Response) {
 
     if (!username || !password) {
         res.status(400).json({ message: "Invalid username or password." });
-    }
+    } else {
 
-    try {
-        const userRecord = await prisma.user.findUnique({
-            where: {
-                username: username,
-            },
-        });
+        try {
+            const userRecord = await prisma.user.findUnique({
+                where: {
+                    username: username,
+                },
+            });
 
-        if (userRecord && await bcrypt.compare(password, userRecord.password)) {
-            res.status(200).json({ username: userRecord.username, email: userRecord.email });
-        } else {
-            res.status(400).json({ message: "Invalid username or password." });
+            if (userRecord && await bcrypt.compare(password, userRecord.password)) {
+                res.status(200).json({ username: userRecord.username, email: userRecord.email });
+            } else {
+                res.status(400).json({ message: "Invalid username or password." });
+            }
+        } catch (err) {
+            res.status(500).json({ message: "There was an error retrieving a user record", err });
         }
-    } catch (err) {
-        res.status(500).json({ message: "There was an error retrieving a user record", err });
     }
 }
